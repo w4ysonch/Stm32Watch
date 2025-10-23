013597bd2763562bf1b5b171978e15a94264021e
brayzlenx <3072237143@qq.com>
2025-10-23 19:32:31 +0800

    更新 main.c

--- STM32WATCH/Core/Src/main.c
+++ STM32WATCH/Core/Src/main.c
@@ -108,7 +108,8 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
+    HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_13);
+    HAL_Delay(100);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
