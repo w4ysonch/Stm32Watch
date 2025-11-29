82808d94bad31321408f33fcac06bfe34104c231
Wayson Chan <3072237143@qq.com>
2025-11-29 23:03:05 +0800

    Release1.2版本
    ①更改了StartFlashTask函数，修复了torchapp只能进入一次的bug

--- STM32WATCH1.2/mytasks/ShowFlashLight.c
+++ STM32WATCH1.2/mytasks/ShowFlashLight.c
@@ -18,7 +18,7 @@ extern TaskHandle_t xShowMenuTaskHandle;
 void ShowFlashLightTask(void *params)
 {
 	buzzer_init();
-	/* ´´½¨¶ÓÁÐ */
+	/* ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ */
 	//g_xQueueMenu = xQueueCreate(1, 4);
 	if(NULL != g_xQueueMenu)HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
 
@@ -33,7 +33,7 @@ void ShowFlashLightTask(void *params)
 	while(1)
 	{
 		// OLED_Clear(); // Don't clear if we want to keep the light on in case 0
-		/* ¶Á°´¼üÖÐ¶Ï¶ÓÁÐ */
+		/* ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ð¶Ï¶ï¿½ï¿½ï¿½ */
 		xQueueReceive(g_xQueueMenu, &key_data, portMAX_DELAY);
 		
 		if(key_data.updata == 1)
@@ -58,6 +58,10 @@ void ShowFlashLightTask(void *params)
 			OLED_Clear(); // Ensure screen is cleared before leaving
 			vTaskResume(xShowMenuTaskHandle);
 			vTaskSuspend(NULL);
+                        light_flag = 0;
+                        OLED_Clear();
+                        OLED_ShowString(20, 3, "Light: OFF", 16, 0);
+                        key_data.exdata = 0;
 		}		
 	}
 }
